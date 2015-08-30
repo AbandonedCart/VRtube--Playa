77c922d645e4fc666ea369671a497baa0846958d
TwistedUmbrella <twistedumbrella@gmail.com>
2015-08-30 17:28:22 -0400

    Add VRGUI to handle interactive floating dialogs

--- Assets/Scripts/AndroidLoader.cs
+++ Assets/Scripts/AndroidLoader.cs
@@ -3,25 +3,41 @@ using System.Collections;
 using QuaternionSoft.Q3D.UnityPlayer;
 using System.IO;
 
-public class AndroidLoader : MonoBehaviour {
+public class AndroidLoader : VRGUI {
 
 	private Q3DRenderer rendererQ3D;
+	private string[] q3dFiles;
 	[SerializeField] private AudioSource audioSource;
+	private int index = 0;
 
+	private string GetPathToExternalData() {
+#if UNITY_ANDROID && !UNITY_EDITOR
+		using (AndroidJavaClass cls_UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+			using (AndroidJavaObject obj_Activity = cls_UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity")) {
+				using(AndroidJavaObject fileDir = obj_Activity.Call<AndroidJavaObject>("getExternalFilesDir", "")) {
+					return fileDir.Call<string>("getAbsolutePath");
+				}
+			}
+		}
+#endif
+		return Application.persistentDataPath;
+	}
+	
 	// Use this for initialization
 	void Start () {
+		q3dFiles = Directory.GetFiles(GetPathToExternalData() + "/", "*.q3d");
 		rendererQ3D = GetComponentInParent<Q3DRenderer> ();
-		string[] q3dFiles = Directory.GetFiles(Application.persistentDataPath + "/", "*.q3d");
 		if (q3dFiles != null && q3dFiles.Length > 0) {
-			if (File.Exists (q3dFiles [0].Replace (".q3d", ".wav"))) {
-				StartCoroutine (loadFile (q3dFiles [0]));
+			if (File.Exists (q3dFiles [index].Replace (".q3d", ".wav"))) {
+				StartCoroutine (loadFile (q3dFiles [index]));
 			} else {
-				rendererQ3D.Filename = q3dFiles [0];
-				rendererQ3D.LoadFile (q3dFiles [0]);
+				rendererQ3D.Filename = q3dFiles [index];
+				rendererQ3D.LoadFile (q3dFiles [index]);
 				rendererQ3D.AutoPlay = true;
 			}
 		} else {
 			// TODO: Warn user we have nothing to play
+			rendererQ3D.StopAllCoroutines();
 			Application.Quit ();
 		}
 	}
@@ -49,5 +65,84 @@ public class AndroidLoader : MonoBehaviour {
 		if (rendererQ3D.IsPlaying) {
 			rendererQ3D.AutoPlay = false;
 		}
+	}
+
+	private int aspectRatio() {
+		if (Camera.main.aspect >= 1.7)
+		{
+			return 2;
+		}
+		else if (Camera.main.aspect >= 1.5)
+		{
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+	
+	private float baseWidth() {
+		return 	854.0f;
+	}
+	
+	private float baseHeight() {
+		if (aspectRatio() == 1) {
+			return 640.0f;
+		} else {
+			return 480.0f;
+		}
+	}
+	
+	public override void OnVRGUI()
+	{
+		Vector3 scale = new Vector3 (
+			Screen.width / baseWidth(),
+			Screen.height / baseHeight(),
+			1f
+			);
+		Matrix4x4 svMat = GUI.matrix;
+		GUI.matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.identity, scale);
+		GUI.skin.label.alignment = TextAnchor.UpperLeft;
+		GUI.skin.button.alignment = TextAnchor.MiddleCenter;
+		GUI.skin.textField.alignment = TextAnchor.MiddleCenter;
+		GUI.skin.label.fontSize = 12;
+		GUI.skin.button.fontSize = 12;
+		GUI.backgroundColor = Color.white;
+		GUI.color = Color.white;
+		
+		if (GUI.Button (new Rect (0, 0, 90, 40), "Previous") || Input.GetKeyDown (KeyCode.LeftArrow)) {
+			index -= 1;
+			if (q3dFiles != null && index >= 0) {
+				rendererQ3D.StopAllCoroutines();
+				if (File.Exists (q3dFiles [index].Replace (".q3d", ".wav"))) {
+					StartCoroutine (loadFile (q3dFiles [index]));
+				} else {
+					rendererQ3D.Filename = q3dFiles [index];
+					rendererQ3D.LoadFile (q3dFiles [index]);
+					rendererQ3D.AutoPlay = true;
+				}
+			} else {
+				index += 1;
+			}
+		}
+
+		if (GUI.Button (new Rect (744, 0, 90, 40), "Next") || Input.GetKeyDown (KeyCode.RightArrow)) {
+			index += 1;
+			if (q3dFiles != null && q3dFiles.Length > index) {
+				rendererQ3D.StopAllCoroutines();
+				if (File.Exists (q3dFiles [index].Replace (".q3d", ".wav"))) {
+					StartCoroutine (loadFile (q3dFiles [index]));
+				} else {
+					rendererQ3D.Filename = q3dFiles [index];
+					rendererQ3D.LoadFile (q3dFiles [index]);
+					rendererQ3D.AutoPlay = true;
+				}
+			} else {
+				index -= 1;
+			}
+		}
+		
+		GUI.matrix = svMat;
 	}
 }
---
+++ Assets/VRGUI/Scripts/VRGUI.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class VRGUI : MonoBehaviour 
+{
+	public Vector3 guiPosition      = new Vector3(0f, 0f, 1f);
+	public float   guiSize          = 1f;
+	public bool    useCurvedSurface = true;
+	public bool    acceptMouse      = true;
+	public bool    acceptKeyboard   = true;
+	public int     cursorSize       = 32;
+	public Texture customCursor     = null;
+	
+	private GameObject    guiRenderPlane    = null;
+	private RenderTexture guiRenderTexture  = null;
+	private Vector2       cursorPosition    = Vector2.zero;
+	private Texture       cursor            = null;
+	
+	private bool isInitialized = false;
+	
+	private void Initialize () 
+	{
+		// create the render plane
+		if (useCurvedSurface)
+		{
+			guiRenderPlane = Instantiate(Resources.Load("VRGUICurvedSurface")) as GameObject;
+		}
+		else
+		{
+			guiRenderPlane = Instantiate(Resources.Load("VRGUIFlatSurface")) as GameObject;
+		}
+		
+		// position the render plane
+		guiRenderPlane.transform.parent        = this.transform;
+		guiRenderPlane.transform.localPosition = guiPosition;
+		if (transform.rotation.y == 180f) {
+			guiRenderPlane.transform.localRotation = Quaternion.identity;
+		} else {
+			guiRenderPlane.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+		}
+		guiRenderPlane.transform.localScale    = new Vector3(guiSize, guiSize, guiSize);
+		
+		// create the render texture
+		guiRenderTexture = new RenderTexture(Screen.width, Screen.height, 24);
+		
+		// assign the render texture to the render plane
+		guiRenderPlane.GetComponent<Renderer>().material.mainTexture = guiRenderTexture;
+		
+		if (acceptMouse)
+		{
+			loadCursorGraphic();
+		}
+		isInitialized = true;
+	}
+	
+	protected void OnEnable()
+	{
+		if (guiRenderPlane != null)
+		{
+			guiRenderPlane.SetActive(true);
+		}
+	}
+	
+	protected void OnDisable()
+	{
+		if (guiRenderPlane != null)
+		{
+			guiRenderTexture.DiscardContents();
+			guiRenderPlane.SetActive(false);
+		}
+	}
+
+	private void loadCursorGraphic() {
+		if (cursor == null) {
+			// create the cursor
+			if (customCursor != null)
+			{
+				cursor = customCursor;
+			}
+			else
+			{
+				cursor = Resources.Load("Textures/SimpleCursor") as Texture;
+			}
+		}
+	}
+	
+	protected void OnGUI()
+	{
+		if (!isInitialized) {
+			Initialize();
+		}
+		
+		// handle mouse events
+		if (Event.current.isMouse)
+		{
+			// return if not accepting mouse events
+			if (!acceptMouse)
+			{
+				return;
+			}
+		}
+		if (acceptMouse)
+		{
+			loadCursorGraphic();
+			// save the mouse position
+			cursorPosition = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+		}
+		
+		// handle key events
+		if (Event.current.isKey)
+		{
+			// return if not accepting key events
+			if (!acceptKeyboard)
+			{
+				return;
+			}
+		}
+		
+		// save current render texture
+		RenderTexture tempRenderTexture = RenderTexture.active; 
+		
+		// set the render texture to render the GUI onto
+		if (Event.current.type == EventType.Repaint)
+		{
+			RenderTexture.active = guiRenderTexture;
+			GL.Clear (true, true, Color.clear, 1.0f);
+		}
+		
+		// draw the VRGUI
+		OnVRGUI();
+		
+		if (Event.current.type == EventType.Repaint)
+		{	
+			if (acceptMouse)
+			{
+				// draw the cursor
+				GUI.DrawTexture(new Rect(cursorPosition.x, cursorPosition.y, cursorSize, cursorSize), cursor, ScaleMode.StretchToFill);
+			}
+			
+			// restore the previous render texture
+			RenderTexture.active = tempRenderTexture;
+		}
+	}
+	
+	public abstract void OnVRGUI();
+}
